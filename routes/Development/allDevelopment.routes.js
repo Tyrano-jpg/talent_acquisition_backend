4c887e6a82e0ed82c2e268b54fc5da2e7b91db96
Tyrano-jpg <tanmay.kdc@gmail.com>
2025-05-15 11:08:00 +0530

    files deleted

--- routes/Development/allDevelopment.routes.js
+++ routes/Development/allDevelopment.routes.js
@@ -1,12 +1,6 @@
 import express from 'express';
 import srMernRouter from './SrMern/srMernRoutes.js';
 import jrMernRouter from './JrMern/jrMernRoutes.js';
-import srSoftEngRouter from './srSoftEng/srSoftEngRoutes.js';
-import jrSoftEngRouter from './jrSoftEng/jrSoftEngRoutes.js';
-import phpRouter from './Php/phpRoutes.js';
-import jrflutterRouter from './jrFlutter/jrFlutterRoutes.js';
-import srflutterRouter from './srFlutter/srFlutterRoutes.js';
-import androidRouter from './Android/androidRoutes.js';
 
 const allDevelopmentRouter = express.Router();
 
@@ -14,12 +8,6 @@ console.log("jsfjsbfjs")
 
 allDevelopmentRouter.use('/sr-mern', srMernRouter)
 allDevelopmentRouter.use('/jr-mern', jrMernRouter)
-allDevelopmentRouter.use('/sr-soft-eng', srSoftEngRouter)
-allDevelopmentRouter.use('/jr-soft-eng', jrSoftEngRouter)
-allDevelopmentRouter.use('php', phpRouter)
-allDevelopmentRouter.use('/jr-flutter', jrflutterRouter)
-allDevelopmentRouter.use('/sr-flutter', srflutterRouter)
-allDevelopmentRouter.use('android', androidRouter)
 
 
 export default allDevelopmentRouter;
