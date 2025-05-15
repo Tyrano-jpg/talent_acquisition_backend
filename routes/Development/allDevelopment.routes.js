6df36a97255e1c6f264cfd3d093be62448b99331
Tyrano-jpg <tanmay.kdc@gmail.com>
2025-05-15 15:35:54 +0530

    route fix

--- routes/Development/allDevelopment.routes.js
+++ routes/Development/allDevelopment.routes.js
@@ -2,7 +2,8 @@ import express from 'express';
 import srMernRouter from './SrMern/srMernRoutes.js';
 import jrMernRouter from './JrMern/jrMernRoutes.js';
 import jrSoftEngRouter from './JrSoftEng/jrSoftEngRoutes.js';
-import srSoftEngRouter from './srSoftEng/srSoftEngRoutes.js';
+// import srSoftEngRouter from './srSoftEng/srSoftEngRoutes.js';
+import srSoftEngRouter from './SrSoftEng/srSoftEngRoutes.js';
 import phpRouter from './Php/phpRoutes.js';
 import jrFlutterRouter from './JrFlutterDev/jrFlutterRoutes.js';
 import srFlutterRouter from './SrFlutterDev/srFlutterRoutes.js';
@@ -14,7 +15,7 @@ console.log("jsfjsbfjs")
 allDevelopmentRouter.use('/sr-mern', srMernRouter)
 allDevelopmentRouter.use('/jr-mern', jrMernRouter)
 allDevelopmentRouter.use('/jr-soft-eng', jrSoftEngRouter)
-allDevelopmentRouter.use('/sr-soft-eng', srSoftEngRouter)
+// allDevelopmentRouter.use('/sr-soft-eng', srSoftEngRouter)
 allDevelopmentRouter.use('/sr-soft-eng', srSoftEngRouter)
 allDevelopmentRouter.use('/php', phpRouter)
 allDevelopmentRouter.use('/jr-flutter', jrFlutterRouter)
