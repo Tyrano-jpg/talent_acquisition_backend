bbc1807681d0b72c4c8723166d8e85c8a4cc8049
Tyrano-jpg <tanmay.kdc@gmail.com>
2025-05-23 15:23:21 +0530

    search fix

--- utils/dynamicSearch/dynamic.js
+++ utils/dynamicSearch/dynamic.js
@@ -5,36 +5,42 @@ export const DynamicSearch = (
   string = [],
   arrayField = []
 ) => {
-  if (search != '') {
+  if (search !== '') {
     let dynamicSearchQueries = [];
+
     let object = [...string, ...numbers];
-    let array = [...arrayField];
-    object?.length > 0 &&
-      object?.map((field) => {
-        dynamicSearchQueries.push({
-          $expr: {
-            $regexMatch: {
-              input: { $toString: `$${field}` },
-              regex: new RegExp(search.toString()),
-              options: 'i',
+
+    // Safe string/number fields with $convert
+    object?.forEach((field) => {
+      dynamicSearchQueries.push({
+        $expr: {
+          $regexMatch: {
+            input: {
+              $convert: {
+                input: `$${field}`,
+                to: 'string',
+                onError: '',
+                onNull: ''
+              }
             },
-          },
-        });
+            regex: new RegExp(search.toString()),
+            options: 'i'
+          }
+        }
       });
+    });
 
-    array?.length > 0 &&
-      array?.map((field) => {
-        dynamicSearchQueries.push({
-          [field]: { $regex: search, $options: 'i' },
-        });
+    // Handle array fields using $elemMatch with regex (only for array of strings)
+    arrayField?.forEach((field) => {
+      dynamicSearchQueries.push({
+        [field]: {
+          $elemMatch: { $regex: search, $options: 'i' }
+        }
       });
+    });
 
-    const searchQuery =
-      dynamicSearchQueries?.length > 0 ? { $or: dynamicSearchQueries } : [];
-    return searchQuery;
+    return dynamicSearchQueries.length > 0 ? { $or: dynamicSearchQueries } : {};
   } else {
-    return [];
+    return {};
   }
 };
-
-
