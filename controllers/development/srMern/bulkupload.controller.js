fbb005877d01a6ae391d2b0adff112c896cd351f
Aryan Aguiar <aguiararyan@gmail.com>
2025-06-02 16:44:52 +0530

    sr-mern bulkupload fix

--- controllers/development/srMern/bulkupload.controller.js
+++ controllers/development/srMern/bulkupload.controller.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import applicationModel from "../../../database/schema/masters/CandidateApplication.schema.js";
+import { isEmpty } from "bullmq";
 
 /**
  * @desc    Bulk upload data to the database
@@ -20,8 +21,16 @@ export const srmern_bulkUpload = async (req, res) => {
         const cleaned = {};
         for (const key in row) {
           const cleanKey = key.trim().replace(/\uFEFF/g, "");
-          const value = row[key];
-          cleaned[cleanKey] = typeof value === "string" ? value.trim() : value;
+          let value = row[key];
+
+          if (cleanKey === "key_skill" && typeof value === "string") {
+            cleaned[cleanKey] = value
+              .split(",")
+              .map(skill => skill.trim())
+              .filter(Boolean); // Remove empty strings
+          } else {
+            cleaned[cleanKey] = typeof value === "string" ? value.trim() : value;
+          }
         }
         return cleaned;
       })
